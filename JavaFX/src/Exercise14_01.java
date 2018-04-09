e4b2e798e7c46f2e53398591382f0db6b954599c
blairaweaver <bw09135@georgiasouthern.edu>
2018-04-09 15:42:09 -0400

    JavaFX Examples and Homework (14.01 & 14.03)

---
+++ JavaFX/src/Exercise14_01.java
@@ -0,0 +1,45 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+
+public class Exercise14_01 extends Application{
+    @Override
+    public void start(Stage primaryStage) {
+//        New GridPane
+        GridPane pane = new GridPane();
+
+//        Get new images from internet and set row and column
+        ImageView image1 = new ImageView("http://liveexample.pearsoncmg.com/book/image/us.gif");
+        GridPane.setConstraints(image1, 0, 0);
+        ImageView image2 = new ImageView("http://liveexample.pearsoncmg.com/book/image/ca.gif");
+        GridPane.setConstraints(image2, 1, 1);
+        ImageView image3 = new ImageView("http://liveexample.pearsoncmg.com/book/image/china.gif");
+        GridPane.setConstraints(image3, 1, 0);
+        ImageView image4 = new ImageView("http://liveexample.pearsoncmg.com/book/image/fr.gif");
+        GridPane.setConstraints(image4, 0, 1);
+
+//        Add all images to gridpane
+        pane.getChildren().addAll(image1,image2, image3, image4);
+
+//        Create new scene and place it in the stage
+        Scene scene = new Scene(pane, 400, 400);
+        primaryStage.setTitle("Test"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ JavaFX/src/Exercise14_03.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Exercise14_03 extends Application{
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
