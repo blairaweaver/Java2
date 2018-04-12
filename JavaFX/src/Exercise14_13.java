e11ab6e3426f1fad8786ff2e7ef973ab9a921f88
blairaweaver <bw09135@georgiasouthern.edu>
2018-04-12 16:07:38 -0400

    Finished 14_13 and started 14_19. Need to add tics to the x-axis on 14_19

--- JavaFX/src/Exercise14_13.java
+++ JavaFX/src/Exercise14_13.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.geometry.Pos;
 
 public class Exercise14_13 extends Application {
     @Override
@@ -11,9 +13,32 @@ public class Exercise14_13 extends Application {
         BorderPane borderPane = new BorderPane();
         Scene scene = new Scene(borderPane, 400, 400);
 
-        Arc quiz = new Arc();
+        double centerX = borderPane.getWidth() / 2;
+        double centerY = borderPane.getHeight() / 2;
+        double quizLength = 360 * 0.1;
+        double projectLength = 360 * 0.2;
+        double midtermLength = 360 * 0.3;
+        double finalsLength = 360 * 0.4;
 
-//        borderPane.setCenter();
+        Arc quiz = new Arc(centerX, centerY, 100, 100, 0.0, quizLength);
+        quiz.setFill(Color.BLUE);
+        quiz.setType(ArcType.ROUND);
+
+        Arc project = new Arc(centerX, centerY, 100, 100, quizLength, projectLength);
+        project.setFill(Color.RED);
+        project.setType(ArcType.ROUND);
+
+        Arc midterm = new Arc(centerX, centerY, 100, 100, quizLength + projectLength, midtermLength);
+        midterm.setFill(Color.DARKGREEN);
+        midterm.setType(ArcType.ROUND);
+
+        Arc finals = new Arc(centerX, centerY, 100, 100, 0.0, -finalsLength);
+        finals.setFill(Color.GOLDENROD);
+        finals.setType(ArcType.ROUND);
+
+        borderPane.getChildren().addAll(quiz, project, midterm, finals, new Text(centerX + 100, centerY - 20, "Quiz -- 10%"),
+                new Text(centerX, centerY - 100, "Project -- 20%"), new Text(centerX - 125, centerY, "Midterm -- 30%"),
+                new Text(centerX + 25, centerY + 125, "Final -- 40%"));
 
         primaryStage.setTitle("Grade Percentages");
         primaryStage.setScene(scene);
---
+++ JavaFX/src/Exercise14_19.java
@@ -0,0 +1,81 @@
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polyline;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+public class Exercise14_19 extends Application {
+    @Override
+    public void start (Stage primaryStage){
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 500, 500);
+
+        double centerX = pane.getWidth() / 2;
+        double centerY = pane.getHeight() / 2;
+
+        drawArrowLine(0, centerY, pane.getWidth(), centerY, pane);
+        drawArrowLine(centerX, pane.getHeight(), centerX, 0, pane);
+
+        Text xLabel = new Text(pane.getWidth() - 20, centerY - 20, "X");
+        Text yLabel = new Text(centerX + 20, 20, "Y");
+        Text
+
+        Polyline sine = new Polyline();
+        Polyline cosine = new Polyline();
+        sine.setStroke(Color.BLUE);
+        cosine.setStroke(Color.RED);
+        ObservableList<Double> sineList = sine.getPoints();
+        ObservableList<Double> cosineList = cosine.getPoints();
+        for (int x = -200; x <= 200; x++){
+            sineList.add(x + centerX);
+//            refer to http://jwilson.coe.uga.edu/EMAT6680Fa07/O'Kelley/Assignment%201/Transformations.html
+//            for transformations of the graph
+            sineList.add(centerY - (centerY / 2) * Math.sin(x / 25.0));
+            cosineList.add(x + centerX);
+            cosineList.add(centerY - (centerY / 2) * Math.cos(x / 25.0));
+        }
+
+        pane.getChildren().addAll(sine, cosine, xLabel, yLabel);
+
+//        System.out.print(centerX)
+
+        primaryStage.setTitle("Sine and Cosine");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void drawArrowLine(double x1, double y1, double x2, double y2, Pane pane) {
+        pane.getChildren().add(new Line(x1, y1, x2, y2));
+
+        // find slope of this line
+        double slope = ((((double) y1) - (double) y2))
+                / (((double) x1) - (((double) x2)));
+
+        double arctan = Math.atan(slope);
+
+        // This will flip the arrow 45 off of a
+        // perpendicular line at pt x2
+        double set45 = 1.57 / 2;
+
+        // arrows should always point towards i, not i+1
+        if (x1 < x2) {
+            // add 90 degrees to arrow lines
+            set45 = -1.57 * 1.5;
+        }
+
+        // set length of arrows
+        int arrlen = 15;
+
+        // draw arrows on line
+        pane.getChildren().add(new Line(x2, y2, (x2 + (Math.cos(arctan + set45) * arrlen)),
+                ((y2)) + (Math.sin(arctan + set45) * arrlen)));
+
+        pane.getChildren().add(new Line(x2, y2, (x2 + (Math.cos(arctan - set45) * arrlen)),
+                ((y2)) + (Math.sin(arctan - set45) * arrlen)));
+    }
+}
